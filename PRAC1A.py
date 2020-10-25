01e4e70308879d40dae534d1cc51003b55e32690
Zainab Shah <jainabkshah05@gmail.com>
2020-10-25 12:25:42 +0530

    Practical 2 (A)
    A. Write A Program To Store The Elements In 1-D Array And Provide An Option To Perform The Operations Like Searching, Sorting, Merging, Reversing The Elements.

--- PRAC1A.py
+++ PRAC1A.py
@@ -1,56 +1,136 @@
-#PRACTICAL 1 (A)
+'''a. Write a program to store the elements in 1-D array and provide an option to 
+perform the operations like searching, sorting, merging, reversing the elements.'''
 
-# Program to add two matrices 
-X = [[11,7,3], 
-      [4 ,5,6], 
-      [7 ,8,9]] 
-    
-Y = [[5,8,1], 
-      [6,7,3], 
-      [4,5,9]] 
+#1) searching
+def binarySearch(arr, low, high, key): 
 
-result = [[0,0,0], 
-          [0,0,0], 
-          [0,0,0]] 
+  
 
-# iterate through rows 
-for i in range(len(X)): 
-# iterate through columns       
-    for j in range(len(X[0])): 
-        result[i][j] = X[i][j] + Y[i][j] 
-        for r in result: 
-            print(r)
-            
-# Program to multiply two matrices
- # 3x3 matrix 
-X = [[12,7,3], 
-     [4 ,5,6], 
-     [7 ,8,9]] 
-# 3x4 matrix 
-Y = [[5,8,1,2], 
-     [6,7,3,0], 
-     [4,5,9,1]] 
-# result is 3x4 
-result = [[0,0,0,0], 
-          [0,0,0,0], 
-          [0,0,0,0]]
- # iterate through rows of X 
-for i in range(len(X)): 
-# iterate through columns of Y 
-    for j in range(len(Y[0])): 
-# iterate through rows of Y 
-        for k in range(len(Y)):      
-            result[i][j] += X[i][k] * Y[k][j] 
-            for r in result: 
-                print(r)
-                
-# Program to transpose a matrix 
-X = [[12,7], [4 ,5], [3 ,8]]
-result = [[0,0,0], [0,0,0]]
- # iterate through rows
-for i in range(len(X)): 
-# iterate through columns 
-    for j in range(len(X[0])): 
-        result[j][i] = X[i][j] 
-        for r in result: 
-            print(r)
+    if (high < low): 
+
+        return -1
+
+    mid = (low + high)/2
+
+  
+
+    if (key == arr[int(mid)]): 
+
+        return mid 
+
+  
+
+    if (key > arr[int(mid)]): 
+
+        return binarySearch(arr, 
+
+           (mid + 1), high, key) 
+
+  
+
+    return (binarySearch(arr, low, 
+
+           (mid -1), key)) 
+
+
+arr = [5, 6, 7, 8, 9, 10] 
+
+n = len(arr) 
+
+key = 10
+
+print("Index:", int(binarySearch(arr, 0, n, key) ))
+
+
+
+#2) Sorting
+
+numbers = [1, 3, 4, 2] 
+# Sorting list of Integers 
+numbers.sort() 
+print(numbers)
+
+
+#3) Merging
+def mergeArrays(arr1, arr2, n1, n2): 
+
+    arr3 = [None] * (n1 + n2) 
+
+    i = 0
+
+    j = 0
+
+    k =0
+
+    while i < n1 and j < n2: 
+
+        if arr1[i] < arr2[j]: 
+
+            arr3[k] = arr1[i] 
+
+            k = k + 1
+
+            i = i + 1
+
+        else: 
+
+            arr3[k] = arr2[j] 
+
+            k = k + 1
+
+            j = j + 1 
+
+    while i < n1: 
+
+        arr3[k] = arr1[i]; 
+
+        k = k + 1
+
+        i = i +1
+
+    while j < n2: 
+
+        arr3[k] = arr2[j]; 
+
+        k = k + 1
+
+        j = j + 1
+
+    print("Array After Merging") 
+
+    for i in range(n1 + n2): 
+
+        print(str(arr3[i]), end = " ") 
+
+  
+
+arr1 = [1, 3, 5, 7] 
+
+n1 = len(arr1) 
+arr2 = [2, 4, 6, 8] 
+
+n2 = len(arr2) 
+mergeArrays(arr1, arr2, n1, n2);
+
+
+
+#4) reverse
+def reverseList(A, start, end):
+
+    while start < end:
+
+        A[start], A[end] = A[end], A[start]
+
+        start += 1
+
+        end -=1
+
+A = [1, 2, 3, 4, 5, 6]
+
+print(A)
+
+reverseList(A, 0, 5)
+
+print("Reversed List is")
+
+print(A)
