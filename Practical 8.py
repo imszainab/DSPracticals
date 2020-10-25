05ef3bd86526f9a8de72ceac3baa0cfce7ba2175
Zainab Shah <jainabkshah05@gmail.com>
2020-10-25 13:58:12 +0530

    Practical 8

---
+++ Practical 8.py
@@ -0,0 +1,75 @@
+#8. Write a program for inorder, postorder and preorder traversal of tree.
+
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.value = key
+
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print(self.value)
+        if self.right:
+            self.right.PrintTree()
+
+    def Printpreorder(self):
+        if self.value:
+            print(self.value)
+            if self.left:
+                self.left.Printpreorder()
+            if self.right:
+                self.right.Printpreorder()
+
+    def Printinorder(self):
+        if self.value:
+            if self.left:
+                self.left.Printinorder()
+            print(self.value)
+            if self.right:
+                self.right.Printinorder()
+
+    def Printpostorder(self):
+        if self.value:
+            if self.left:
+                self.left.Printpostorder()
+            if self.right:
+                self.right.Printpostorder()
+            print(self.value)
+
+    def insert(self, data):
+        if self.value:
+            if data < self.value:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.value:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.value = data
+
+
+if __name__ == '__main__':
+    root = Node(10)
+    root.left = Node(12)
+    root.right = Node(5)
+    print("Without Any Order")
+    root.PrintTree()
+    root_1 = Node(None)
+    root_1.insert(28)
+    root_1.insert(4)
+    root_1.insert(13)
+    root_1.insert(130)
+    root_1.insert(123)
+    print("Now Ordering Tree Elements With Insert")
+    root_1.PrintTree()
+    print("Tree Elements In Pre Order")
+    root_1.Printpreorder()
+    print("Tree Elements: In Order")
+    root_1.Printinorder()
+    print("Tree Elements In Post Order")
+    root_1.Printpostorder()
