2e8f296ba537b0981f5bbae67229de1a6144242c
Zainab Shah <jainabkshah05@gmail.com>
2020-10-25 12:39:22 +0530

    Practical 02 (B)
    b. Write a program to perform the Matrix addition, Multiplication and Transpose
    Operation.

--- PRAC1B.py
+++ PRAC1B.py
@@ -1,54 +1,37 @@
-# Program to add two matrices 
-X = [[11,7,3], 
-      [4 ,5,6], 
-      [7 ,8,9]] 
-    
-Y = [[5,8,1], 
-      [6,7,3], 
-      [4,5,9]] 
+'''Practical 1 (b)Write a program to perform the Matrix addition, Multiplication and Transpose
+Operation.'''
+Matrix1 = [[3, 4, -6], 
+      [12,71,24], 
+      [21,3,21]]
 
-result = [[0,0,0], 
-          [0,0,0], 
-          [0,0,0]] 
+Matrix2 = [[2, 16, -16],
+           [1,7,-3], 
+           [-1,3,3]]
+Matrix3 = [[0,0,0,],
+     [0,0,0,],
+     [0,0,0,]]
 
-# iterate through rows 
-for i in range(len(X)): 
-# iterate through columns       
-    for j in range(len(X[0])): 
-        result[i][j] = X[i][j] + Y[i][j] 
-        for r in result: 
-            print(r)
-            
-# Program to multiply two matrices
- # 3x3 matrix 
-X = [[12,7,3], 
-     [4 ,5,6], 
-     [7 ,8,9]] 
-# 3x4 matrix 
-Y = [[5,8,1,2], 
-     [6,7,3,0], 
-     [4,5,9,1]] 
-# result is 3x4 
-result = [[0,0,0,0], 
-          [0,0,0,0], 
-          [0,0,0,0]]
- # iterate through rows of X 
-for i in range(len(X)): 
-# iterate through columns of Y 
-    for j in range(len(Y[0])): 
-# iterate through rows of Y 
-        for k in range(len(Y)):      
-            result[i][j] += X[i][k] * Y[k][j] 
-            for r in result: 
-                print(r)
-                
-# Program to transpose a matrix 
-X = [[12,7], [4 ,5], [3 ,8]]
-result = [[0,0,0], [0,0,0]]
- # iterate through rows
-for i in range(len(X)): 
-# iterate through columns 
-    for j in range(len(X[0])): 
-        result[j][i] = X[i][j] 
-        for r in result: 
-            print(r)
+# Matrix Addition
+for i in range(len(Matrix1)):
+    for j in range(len(Matrix2[0])):
+        for k in range(len(Matrix2)):
+            Matrix3[i][j] += Matrix1[i][k] + Matrix2[k][j]
+        
+print(Matrix3)
+
+# Matrix Multiplication
+
+Matrix3 = [[0, 0, 0, 0], 
+        [0, 0, 0, 0], 
+        [0, 0, 0, 0]] 
+
+for i in range(len(Matrix1)):
+    for j in range(len(Matrix2[0])):
+        for k in range(len(Matrix2)):
+            Matrix3[i][j] += Matrix1[i][k] * Matrix2[k][j]
+        
+print(Matrix3)
+
+#matrix transpose
+for i in map(list, zip(*Matrix1)):
+    print(i)
